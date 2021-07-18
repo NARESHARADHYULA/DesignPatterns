d3520438033a3e11ed2d363590d05025eeac0d2f
Naresh Aradhyula <naresha@leapthought.co.nz>
2021-07-19 00:54:17 +0530

    Add project files.

---
+++ Singleton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    /// <summary>
+    /// Thread safe Lazy implementation of a singleton class
+    /// untill we call lazy.value class is not instantiated
+    /// lazy.value always takes of null values
+    /// </summary>
+    public sealed class Singleton
+    {
+        public static readonly Lazy<Singleton> _lazy = new(() => new Singleton());
+        private Singleton() { }
+        public static Singleton Instance
+        {
+            get
+            {
+                return _lazy.Value;
+            }
+        }
+    }
+}
